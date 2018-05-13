dd22763e50fd772226febb782b3d8be81416f63a
Peter Schiffer <pschiffe@redhat.com>
2018-05-13 21:25:41 +0200

    Simplify main.go

--- main.go
+++ main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
-  "fmt"
   "net/http"
 )
 
 func main() {
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Welcome to my website!")
+    w.Write([]byte("Welcome to my website!"))
   })
 
   fs := http.FileServer(http.Dir("static/"))
