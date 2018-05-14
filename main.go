84a326369563e823dca25fd756df139c3994c2f4
Peter Schiffer <pschiffe@redhat.com>
2018-05-14 17:10:07 +0200

    Update main.go

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 
 func main() {
   http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte("500 - Something bad happened!"))
+    // w.WriteHeader(http.StatusInternalServerError)
+    w.Write([]byte("working!"))
   })
 
   fs := http.FileServer(http.Dir("static/"))
