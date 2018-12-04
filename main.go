e0da8ef973ae246f8f021425e74a71c77e20612c
Jan Wozniak <jawoznia@cisco.com>
2018-12-04 11:32:30 +0100

    test webhook

--- main.go
+++ main.go
@@ -1,17 +1,17 @@
 package main
 
 import (
-  "net/http"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-    w.WriteHeader(http.StatusInternalServerError)
-    w.Write([]byte("working!"))
-  })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("working with hooks!"))
+	})
 
-  fs := http.FileServer(http.Dir("static/"))
-  http.Handle("/static/", http.StripPrefix("/static/", fs))
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-  http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
