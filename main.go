87c78be7d99c9272e82bf462d1149bdd193871cc
Jan Wozniak <jawoznia@cisco.com>
2018-12-12 16:01:29 +0100

    demo commit

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("working!\n"))
+		w.Write([]byte("working with hooks!\n"))
 	})
 
 	fs := http.FileServer(http.Dir("static/"))
